ff917dff8fba2704e788c9611e49cb9b16d8e382
Anubhav Jha <anubhav.jha@lenskart.in>
2021-04-09 18:36:16 +0530

    Pages created

---
+++ src/components/chat/chat-list/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const index = () => {
+  return (
+    <View>
+      <Text>List of Chats</Text>
+    </View>
+  );
+};
+
+export default index;
